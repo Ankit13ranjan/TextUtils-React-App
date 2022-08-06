02abf38b2dd3b9133442433b1ba82ab9074ce027
Ankit Ranjan <ranjanankit1313@gmail.com>
2022-08-06 20:46:54 +0530

    App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" aboutTextUtils="About TextUtils" />
+    </>
+  );
+}
+
+export default App;
